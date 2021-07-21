12d9eb6fb8b1fc7cc5e43143591cf3e4a2ea3336
Regina <reginatam429@gmail.com>
2021-07-21 03:37:25 -0700

    adds heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-# import os
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
-# load_dotenv()
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    #     "SQLALCHEMY_DATABASE_URI")
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/citymove_api"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    # app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/citymove_api"
 
     db.init_app(app)
     migrate.init_app(app, db)
